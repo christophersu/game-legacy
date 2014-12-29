563471bffbdd355eee75830d443b679e62bf1dbc
Brendan Redmond <bredmond000@gmail.com>
2014-12-28 23:52:30 -0800

    Reviewed AbstractCombatCard.

--- src/main/com/boardgame/game/AbstractCombatCard.java
+++ src/main/com/boardgame/game/AbstractCombatCard.java
@@ -1,19 +1,21 @@
 package com.boardgame.game;
 
-class AbstractCombatCard {
+abstract class AbstractCombatCard {
 	private final String name;
 	private final int strength;
 	private final int killingPotential;
 	private final int deathDefense;
 	
-	AbstractCombatCard(String name, int strength, int killingPotential, 
-			int deathDefense) {
+	AbstractCombatCard(String name, int strength, int killingPotential, int deathDefense) {
+		assert name != null;
+		assert strength >= 0;
+		assert killingPotential >= 0;
+		assert deathDefense >= 0;
+		
 		this.name = name;
 		this.strength = strength;
 		this.killingPotential = killingPotential;
 		this.deathDefense = deathDefense;
-		
-		checkRep();
 	}
 	
 	String getName() {
@@ -30,12 +32,5 @@ class AbstractCombatCard {
 	
 	int getDeathDefense() {
 		return deathDefense;
-	}
-	
-	private void checkRep() {
-		assert name != null : "Null name";
-		assert strength >= 0 : "Negative strength";
-		assert killingPotential >= 0 : "Negative killing potential";
-		assert deathDefense >= 0 : "Negative death defense";
 	}
 }
