70890ef5f386ae9f167a755e226fd50a7c4b1230
Christopher Su <christophersu9@gmail.com>
2015-01-09 22:02:22 -0800

    testing serializer

--- src/main/com/boardgame/game/GameSerializer.java
+++ src/main/com/boardgame/game/GameSerializer.java
@@ -5,13 +5,18 @@ import java.io.IOException;
 import com.cedarsoftware.util.io.JsonWriter;;
 
 public class GameSerializer {
-	String serializeGameStateToJson(Game game) {
+	static String serializeGameStateToJson(GameState gs) {
 		try {
-			return JsonWriter.objectToJson(game.getGameState());
+			return JsonWriter.objectToJson(gs);
 		} catch (IOException e) {
 			// TODO Throw better error here.
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	public static void main(String[] args) {
+		GameState gs = GameStateLoader.load(GameStateLoader.GameType.STANDARD_6);
+		System.out.println(serializeGameStateToJson(gs));
 	}
 }
